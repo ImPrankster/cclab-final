f8eaa0794307e8096734ed3e1134fe638c5a717a
Ruofan Wong <meimprankster@gmail.com>
2022-11-28 15:53:41 +0800

    Add Confetti

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fun: ["Monoton"],
+      },
+      colors: {
+        warm: "#FFFFFB",
+      },
+    },
   },
   plugins: [],
-}
+};
